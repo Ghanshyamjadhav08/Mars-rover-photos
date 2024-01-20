280a82e25238838500339891a9ed9773a6bf7b8b
Ghanshyam Jadhav <141905359+Ghanshyamjadhav08@users.noreply.github.com>
2024-01-20 22:35:39 +0530

    Update nasa.jsx

--- Nasa/nasa.jsx
+++ Nasa/nasa.jsx
@@ -4,7 +4,7 @@ import"./nasa.css";
 export function NasaComponent()
 {
     const [mars, setMars] = useState([]);
-    
+
       useEffect(()=>{
         fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
          .then((response)=>{
@@ -15,7 +15,7 @@ export function NasaComponent()
          })
       },[])
    
-
+//all fetched photos shows in card style
         return(
                 <div className="container-fluid">
                     <h2>Mars Photos</h2>
